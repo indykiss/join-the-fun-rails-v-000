b4105b66198f7a9d0a0eec9d908e4a45aa81d5e0
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-25 22:07:03 +0000

    Automatically backed up by Learn

--- db/migrate/20190225220559_change_columns.rb
+++ db/migrate/20190225220559_change_columns.rb
@@ -2,5 +2,8 @@ class ChangeColumns < ActiveRecord::Migration
   def change
     add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
+    add_column :taxis, :passenger_id, :integer
   end
+
+
 end
